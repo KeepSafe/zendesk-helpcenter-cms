5ab8aa295d70ea3761cebd544a2345e8918b286c
ca77y <ca77y@live.com>
2014-07-17 00:46:32 +0200

    create group translations

--- translator.py
+++ translator.py
@@ -17,9 +17,8 @@ import unicodedata
 
 def slugify(value):
     """
-    Converts to lowercase, removes non-word characters (alphanumerics and
-    underscores) and converts spaces to hyphens. Also strips leading and
-    trailing whitespace.
+    Converts to lowercase, removes non-word characters (alphanumerics and underscores) and converts spaces to
+    hyphens. Also strips leading and trailing whitespace.
     """
     value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
     value = re.sub('[^\w\s-]', '', value).strip().lower()
@@ -40,25 +39,47 @@ LOGGER = Logger()
 
 
 class MetaRepository(object):
+    # TODO possible name conflicts with articles
     META_FILENAME = 'meta.json'
 
     def save_group(self, group, path):
-        file_content = {
-            'name': group['name'],
-            'description': group['description']
-        }
         with open(os.path.join(path, MetaRepository.META_FILENAME), 'w') as file:
-            json.dump(file_content, file, indent=4, sort_keys=True)
+            json.dump(group, file, indent=4, sort_keys=True)
 
     def save_article(self, article, path):
         with open(os.path.join(path, slugify(article['name']) + '.json'), 'w') as file:
             json.dump(article, file, indent=4, sort_keys=True)
 
+    def get_group_id(self, path):
+        meta_path = os.path.join(path, MetaRepository.META_FILENAME)
+        if os.path.exists(meta_path):
+            with open(meta_path, 'r') as file:
+                data = json.load(file)
+                return data['id']
+        else:
+            return None
 
-class ArticleRepository(object):
+    def is_category(self, path):
+        with open(os.path.join(path, MetaRepository.META_FILENAME), 'r') as file:
+            data = json.load(file)
+            return 'category_id' not in data
+
+
+class ContentRepository(object):
+    CONTENT_FILENAME = 'content.json'
+
+    def is_content(self, name):
+        return name == ContentRepository.CONTENT_FILENAME
+
     def save_group(self, group, path):
         group_path = os.path.join(path, slugify(group['name']))
         os.makedirs(group_path, exist_ok=True)
+        group_content = {
+            'name': group['name'],
+            'description': group['description']
+        }
+        with open(os.path.join(path, ContentRepository.CONTENT_FILENAME), 'w') as file:
+            json.dump(group_content, file, indent=4, sort_keys=True)
         return group_path
 
     def save_article(self, article, group_path):
@@ -66,16 +87,34 @@ class ArticleRepository(object):
         with open(os.path.join(group_path, slugify(article['name']) + '.md'), 'w') as file:
             file.write(file_content)
 
+    def get_translated_groups(self, files):
+        result = {}
+        master_name, master_ext = os.path.splitext(ContentRepository.CONTENT_FILENAME)
+        for file in files:
+            if file == ContentRepository.CONTENT_FILENAME:
+                result['en-us'] = file
+            else:
+                name, ext = os.path.splitext(file)
+                if name.startswith(master_name + '.') and ext == master_ext:
+                    locale = name.split('.')[-1]
+                    result[locale] = file
+        return result
+
+    def get_translated_articles(self, files):
+        articles = filter(lambda file: file.endswith('.md'), files)
+        for article in articles:
+            pass
+
 
 class ZendeskClient(object):
     DEFAULT_URL = 'https://{}.zendesk.com/hc/api/v2/{}'
 
-    def __init__(self, company_name):
+    def __init__(self, options):
         super().__init__()
-        self.company_name = company_name
+        self.options = options
 
     def url_for(self, path):
-        return ZendeskClient.DEFAULT_URL.format(self.company_name, path)
+        return ZendeskClient.DEFAULT_URL.format(self.options['company_name'], path)
 
     def fetch_categories(self):
         response = requests.get(self.url_for('categories.json'))
@@ -92,6 +131,39 @@ class ZendeskClient(object):
         response_data = response.json()
         return response_data['articles'] if response_data else []
 
+    def _translate_group(self, url, translations, request_fn):
+        result = []
+        for translation in translations:
+            response = request_fn(url, data=json.dumps({'translation': translation}),
+                                  auth=(self.options['user'], self.options['password']),
+                                  headers={'Content-type': 'application/json'})
+            response_data = response.json()
+            print(translation)
+            result.append(response_data['translation'])
+        return result
+
+    def create_category_translation(self, category_id, translations):
+        url = self.url_for('categories/{}/translations.json'.format(category_id))
+        return self._translate_group(url, translations, requests.post)
+
+    def create_section_translation(self, section_id, translations):
+        url = self.url_for('sections/{}/translations.json'.format(section_id))
+        return self._translate_group(url, translations, requests.post)
+
+    def create_article_translation(self):
+        pass
+
+    def update_category_translation(self, category_id, translations):
+        url = self.url_for('categories/{}/translations.json'.format(category_id))
+        return self._translate_group(url, translations, requests.put)
+
+    def update_section_translation(self, section_id, translations):
+        url = self.url_for('sections/{}/translations.json'.format(section_id))
+        return self._translate_group(url, translations, requests.put)
+
+    def update_article_translation(self):
+        pass
+
 
 class WebTranslateItClient(object):
     DEFAULT_URL = 'https://webtranslateit.com/api/projects/{}/{}'
@@ -103,19 +175,17 @@ class WebTranslateItClient(object):
     def create_file(self, filepath):
         with open(filepath, 'r') as file:
             linux_filepath = filepath.replace('\\', '/')
-            res = requests.post(self.url_for('files'),
-                                data={'file': linux_filepath, 'name': linux_filepath},
-                                files={'file': file})
-            print(linux_filepath)
-            print(res.text)
+            # TODO handle response
+            response = requests.post(self.url_for('files'), data={'file': linux_filepath, 'name': linux_filepath},
+                                     files={'file': file})
 
 
 class ImportTask(object):
     def __init__(self, options):
         super().__init__()
         self.options = options
-        self.zendesk = ZendeskClient(options['company_name'])
-        self.repository = ArticleRepository()
+        self.zendesk = ZendeskClient(options)
+        self.repository = ContentRepository()
         self.meta = MetaRepository()
 
     def execute(self):
@@ -145,9 +215,42 @@ class ImportTask(object):
 class ExportTask(object):
     def __init__(self, options):
         super().__init__()
+        self.options = options
+        self.zendesk = ZendeskClient(options)
+        self.meta = MetaRepository()
+        self.repo = ContentRepository()
 
     def execute(self):
-        print('export task')
+        for root, _, files in os.walk(self.options['root_folder']):
+            group_id = self.meta.get_group_id(root)
+            # TODO every folder should have this, if not it's a new group
+            if group_id:
+                groups = self._translated_groups(root, files)
+                # if self.meta.is_category(root):
+                # self.zendesk.create_category_translation(group_id, groups)
+                # else:
+                #     self.zendesk.create_section_translation(group_id, groups)
+            articles = self._translated_articles(files)
+
+    def _translated_groups(self, path, files):
+        groups = self.repo.get_translated_groups(files)
+        result = []
+        for locale, filename in groups.items():
+            with open(os.path.join(path, filename), 'r') as file:
+                file_data = json.load(file)
+                translation = {
+                    'title': file_data['name'],
+                    'body': file_data['description'],
+                    'locale': locale
+                }
+                result.append(translation)
+        return result
+
+    def _translated_articles(self, files):
+        result = []
+        articles = self.repo.get_translated_articles(files)
+
+        return result
 
 
 class TranslateTask(object):
@@ -157,17 +260,17 @@ class TranslateTask(object):
         super().__init__()
         self.options = options
         self.translate = WebTranslateItClient()
+        self.meta = MetaRepository()
+        self.repo = ContentRepository()
 
-    def _is_file_to_translate(self, path):
+    def is_file_to_translate(self, path):
         _, ext = os.path.splitext(path)
         name = os.path.basename(path)
-        return ext in TranslateTask.TRANSLATE_EXTENSIONS or name == MetaRepository.META_FILENAME
+        return ext in TranslateTask.TRANSLATE_EXTENSIONS or self.repo.is_content(name)
 
     def execute(self):
-
         for root, _, files in os.walk(self.options['root_folder']):
-
-            files = filter(self._is_file_to_translate, files)
+            files = filter(self.is_file_to_translate, files)
             for file in files:
                 self.translate.create_file(os.path.join(root, file))
 
@@ -197,8 +300,10 @@ def parse_args():
 
 def parse_options():
     return {
+        'root_folder': 'help_center_content',
         'company_name': 'testingzendesk12',
-        'root_folder': 'help_center_content'
+        'user': 'zendesk@maildrop.cc',
+        'password': '123zendesk'
     }
 
 
